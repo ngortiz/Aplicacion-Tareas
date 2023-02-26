a08f334e1e83efe79e920e1bd4f996e9b317aa83
ngortiz <nidigortiz21@gmail.com>
2023-02-26 11:54:14 -0300

    nuevas modificaciones

--- src/componentes/Tarea.js
+++ src/componentes/Tarea.js
@@ -1,16 +1,14 @@
-import './App.css';
-import freeCodeCampLogo from './imagenes/freecodecamp-logo.png';
-import ListaDeTareas from './componentes/ListaDeTareas';
+import "../hojas-de-estilos/Tarea.css";
+import freeCodeCampLogo from "../imagenes/freecodecamp-logo.png";
+import ListaDeTareas from "./ListaDeTareas";
 
 function App() {
   return (
-    <div className='aplicacion-tareas'>
-      <div className='freecodecamp-logo-contenedor'>
-        <img 
-          src={freeCodeCampLogo} 
-          className='freecodecamp-logo' />
+    <div className="aplicacion-tareas">
+      <div className="freecodecamp-logo-contenedor">
+        <img src={freeCodeCampLogo} className="freecodecamp-logo" alt="logo" />
       </div>
-      <div className='tareas-lista-principal'>
+      <div className="tareas-lista-principal">
         <h1>Mis Tareas</h1>
         <ListaDeTareas />
       </div>
--- src/componentes/TareaFormulario.js
+++ src/componentes/TareaFormulario.js
@@ -1,41 +1,36 @@
-import React, { useState } from 'react';
-import '../hojas-de-estilo/TareaFormulario.css';
-import { v4 as uuidv4 } from 'uuid';
+import React, { useState } from "react";
+import "../hojas-de-estilos/TareaFormulario.css";
+import { v4 as uuidv4 } from "uuid";
 
 function TareaFormulario(props) {
+  const [input, setInput] = useState("");
 
-  const [input, setInput] = useState('');
-
-  const manejarCambio = e => {
+  const manejarCambio = (e) => {
     setInput(e.target.value);
-  }
+  };
 
-  const manejarEnvio = e => {
+  const manejarEnvio = (e) => {
     e.preventDefault();
-    
+
     const tareaNueva = {
       id: uuidv4(),
       texto: input,
-      completada: false
-    }
+      completada: false,
+    };
 
     props.onSubmit(tareaNueva);
-  }
+  };
 
   return (
-    <form 
-      className='tarea-formulario'
-      onSubmit={manejarEnvio}>
-      <input 
-        className='tarea-input'
-        type='text'
-        placeholder='Escribe una Tarea'
-        name='texto'
+    <form className="tarea-formulario" onSubmit={manejarEnvio}>
+      <input
+        className="tarea-input"
+        type="text"
+        placeholder="Escribe una Tarea"
+        name="texto"
         onChange={manejarCambio}
       />
-      <button className='tarea-boton'>
-        Agregar Tarea
-      </button>
+      <button className="tarea-boton">Agregar Tarea</button>
     </form>
   );
 }
